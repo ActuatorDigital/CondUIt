8a25a1d48883edbe8b9866331d24857f7987ac09
Lewis <lreidwilliams@gmail.com>
2017-05-08 09:35:23 +1000

    added transition handlers

---
+++ Assets/MVC/View/Transitions/OnOffTransition.cs
@@ -0,0 +1,21 @@
+﻿namespace MVC.Views
+{
+    public class OnOffTransition : TransitionHandler
+    {
+        public override void OnHide()
+        {
+            SetActive(false);
+        }
+
+        public override void OnShow()
+        {
+            SetActive(true);
+        }
+
+        private void SetActive(bool isActive)
+        {
+            gameObject.SetActive(isActive);
+        }
+    }
+}
+
---
+++ Assets/MVC/View/Transitions/TransitionEventTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace MVC.Views
+{
+    public class TransitionEventTrigger : TransitionHandler
+    {
+        [SerializeField]
+        private UnityEvent _showHandlers;
+        [SerializeField]
+        private UnityEvent _hideHandlers;
+
+        public override void OnHide()
+        {
+            _hideHandlers.Invoke();
+        }
+
+        public override void OnShow()
+        {
+            _showHandlers.Invoke();
+        }
+    }
+}
+

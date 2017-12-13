207faf49d998f755dcd695c91f14e68fa5f92b3d
Sebastiaan Fehr <SebFehr@animaginedreality.com>
2017-12-14 00:21:08 +1000

    Removed JSON from model.

--- Assets/MVC/Model/Model.cs
+++ Assets/MVC/Model/Model.cs
@@ -1,12 +1,9 @@
 ﻿
-using JsonFx.Json;
-
 namespace MVC {
 
     public abstract class Model<P> : IModel
             where P : IModel {
-
-        [JsonIgnore]
+        
         public P Parent { get; set; }
         public Model() { }
         public Model(P parent) {

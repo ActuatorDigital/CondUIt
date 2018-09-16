f195716d5df434aa1099ddfd1626c684d630ad91
Sebastiaan Fehr <Seb@animaginedreality.com>
2018-09-16 14:59:22 +1000

    Added context menu options from which cretion menus will be triggered

---
+++ Assets/Editor/MVC/MvcContextMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MvcContextMenu : MonoBehaviour {
+
+	[MenuItem("GameObject/UI/MVC/Controller")]
+	static void CreateController(){
+		UnityEngine.Debug.Log("Show Controller creation menu here");
+	}
+
+	[MenuItem("GameObject/UI/MVC/View")]
+	static void CreateView(){
+		UnityEngine.Debug.Log("Show View Creation menu here.");
+	}
+
+
+	[MenuItem("GameObject/UI/MVC/Model")]
+	static void CreateModel(){
+		UnityEngine.Debug.Log("Show Model Creation menu here.");
+	}
+
+}

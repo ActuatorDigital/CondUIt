0da85e664f096a77df0c32bab89bcdcdfa102cc9
Sebastiaan Fehr <seb@animaginedreality.com>
2018-09-20 18:31:12 +1000

    Add IMGUI support through OnDebugGUI() to DebugService, so that devs can easily add comples behaviour to their debug services.

--- Assets/MVC/DebugService.cs
+++ Assets/MVC/DebugService.cs
@@ -1,44 +1,60 @@
 ﻿using System;
 using System.Collections;
+using UnityEditor;
 using UnityEngine;
 
-public abstract class DebugService : MonoBehaviour {
+namespace Services {
+    public abstract class DebugService : MonoBehaviour {
 
-    [SerializeField]
-    private float _loadingDelay = 0.5f;
+        [SerializeField]
+        private float _loadingDelay = 0.5f;
 
-    public static DebugService Create<T> () where T : DebugService, new() 
-    {
-        var existingService = FindObjectOfType<DebugService>();
-        var parentGO = existingService == null ? new GameObject("Debug Services") : 
-                       existingService.transform.parent.gameObject;
+        public virtual void OnDebugGUI(){}
 
-        var go = new GameObject(typeof(T).Name);
-        go.transform.SetParent(parentGO.transform);
-        return go.AddComponent<T>();
-    }
-
-    public static Action<float> EmptyProgressHandler { get { return (p) => { }; } }
-
-    protected void Process(Action callback, Action<float> progress) {
-        StartCoroutine(ProcessRequest(callback, progress, _loadingDelay));
-    }
-
-    protected void Process(Action callback, Action<float> progress, float duration)
-    {
-        StartCoroutine(ProcessRequest(callback, progress, duration));
-    }
-
-    private IEnumerator ProcessRequest (Action callback, Action<float> progressHandler, float duration)
-    {
-        float t = 0f;
-        while(t < duration && duration > 0)
+        public static T Create<T> () where T : DebugService, new() 
         {
-            yield return null;
-            t += Time.deltaTime;
-            progressHandler.Invoke(t / duration);
+            var existingService = FindObjectOfType<DebugService>();
+            var parentGO = existingService == null ? new GameObject("Debug Services") : 
+                        existingService.transform.parent.gameObject;
+
+            var go = new GameObject(typeof(T).Name);
+            go.transform.SetParent(parentGO.transform);
+            return go.AddComponent<T>();
         }
 
-        callback.Invoke();
+        public static Action<float> EmptyProgressHandler { get { return (p) => { }; } }
+
+        protected void Process(Action callback, Action<float> progress) {
+            StartCoroutine(ProcessRequest(callback, progress, _loadingDelay));
+        }
+
+        protected void Process(Action callback, Action<float> progress, float duration)
+        {
+            StartCoroutine(ProcessRequest(callback, progress, duration));
+        }
+
+        private IEnumerator ProcessRequest (Action callback, Action<float> progressHandler, float duration)
+        {
+            float t = 0f;
+            while(t < duration && duration > 0)
+            {
+                yield return null;
+                t += Time.deltaTime;
+                progressHandler.Invoke(t / duration);
+            }
+
+            callback.Invoke();
+        }
     }
+
+    
+    [CustomEditor(typeof(DebugService), true)]
+    public class DebugEditor : Editor {
+        public override void OnInspectorGUI(){
+            DrawDefaultInspector();
+            var debugService = (DebugService)target;
+            debugService.OnDebugGUI();
+        }
+    }
+
 }
